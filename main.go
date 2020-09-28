6fd3f447272a0a6aa85574527c1b76d3aee8b67f
Christian De la Cruz Belmonte <christian.delacruz6257@alumnos.udg.mx>
2020-09-28 08:10:05 -0500

    Se agrego el codigo para solucionar el problema

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func factorial(n int) uint32 {
+	var fact uint32 = 1
+	for i := 2; i <= n; i++ {
+		fact *= uint32(i)
+	}
+	return fact
+}
+
+func main() {
+	var e float64
+	var max uint8
+
+	fmt.Scanln(&max)
+
+	for i := 0; i < int(max); i++ {
+		e += 1.0 / float64(factorial(i))
+	}
+
+	fmt.Println(e)
 }
